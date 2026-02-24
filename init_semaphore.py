b6a8bd43c5d58e34b9ac1f46745ae399cdb77e15
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-02-24 07:41:12 -0600

    fix restart issue

--- init_semaphore.py
+++ init_semaphore.py
@@ -1,5 +1,5 @@
 from utils import get_threads_config
-from posix_ipc import Semaphore, O_CREX
+from posix_ipc import Semaphore, O_CREAT
 
 # Init the scan thread semaphore
-Semaphore(f"/cxone_scan", flags=O_CREX, initial_value=get_threads_config())
+Semaphore(f"/cxone_scan", flags=O_CREAT, initial_value=get_threads_config())
