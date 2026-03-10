f7cc07ccbd741b95b8704130e0b6ebc5aa8dba54
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-03-10 14:31:46 -0400

    cleanup

--- scheduler.py
+++ scheduler.py
@@ -10,7 +10,7 @@ else:
     is_audit = False
     utils.configure_normal_logging()
 
-import asyncio, aiofiles, time
+import asyncio, time
 from cxone_api import CxOneClient
 from cxone_api.exceptions import CommunicationException
 from logic import Scheduler
