5e2681b68e6cd9114c0a09cf94342c1df59cbd7f
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-05-19 12:49:44 -0500

    ca compat with k8s

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cp /etc/ssl/certs/ca-certificates.crt /opt/cxone/certs/certs.crt
 
 export REQUESTS_CA_BUNDLE=/opt/cxone/certs/certs.crt
 
-for cert in $(find /usr/share/ca-certificates -maxdepth 1 -name '*.crt' -print);
+for cert in $(find /usr/local/share/ca-certificates -maxdepth 1 -name '*.crt' -print);
 do
     if [[ -n "$LOG_LEVEL" && $LOG_LEVEL == "DEBUG" ]]; then
         echo Adding $cert as a trusted CA certificate...
